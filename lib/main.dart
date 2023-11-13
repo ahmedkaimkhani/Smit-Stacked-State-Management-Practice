6089c7ce5da4c195970df155d9ffc7855031f5e8
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-14 02:25:01 +0500

    Stacked

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:stacked_management_practice/views/home_view.dart';
+import 'counter/views/home_view.dart';
 
 void main() {
   runApp(const MyApp());
