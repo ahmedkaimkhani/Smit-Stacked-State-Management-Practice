2a72558e7c522b93c42752a5381d0175c59c59d8
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-14 05:16:45 +0500

    Stacked

--- lib/counter app/views/counter_view.dart
+++ lib/counter app/views/counter_view.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
 
+import '../view model/counter_viewmodel.dart';
+
+class CounterView extends StatelessWidget {
+  const CounterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder.reactive(
+      viewModelBuilder: () => CounterViewModel(),
+      builder: (context, viewModel, child) {
+        return const Scaffold();
+      },
+    );
+  }
+}
