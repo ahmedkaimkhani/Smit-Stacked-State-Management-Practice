2c4a519c7ec690c3eca0c77f62a6c58228ce6495
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-14 06:39:35 +0500

    Stacked

---
+++ lib/counter app/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:stacked/stacked_annotations.dart';
+import 'package:stacked_management_practice/counter%20app/services/counter_service.dart';
+import 'package:stacked_management_practice/counter%20app/views/counter_view.dart';
+import 'package:stacked_management_practice/counter%20app/views/home_views.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+@StackedApp(routes: [
+  MaterialRoute(page: CounterView),
+  MaterialRoute(page: HomeViews),
+], dependencies: [
+  Singleton(classType: NavigationService),
+  LazySingleton(classType: CounterService)
+])
+class App {}
